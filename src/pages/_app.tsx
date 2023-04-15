477794a9fb3744cef43e38e05193a240f1f7291c
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-15 10:41:01 -0300

    fix: general styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import { type AppType } from "next/app";
 import { ClerkProvider } from "@clerk/nextjs";
 import { api } from "~/utils/api";
 
+import { NavBar } from "~/components/NavBar";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />;
+      <div className="dark:text-white light:black" >
+        <NavBar />
+        <Component {...pageProps} />;
+      </div>
     </ClerkProvider>
   );
 };
