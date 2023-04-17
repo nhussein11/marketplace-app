aa4660b6bb09dc0f96a6da3689166d0eba876154
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-17 10:18:33 -0300

    feat: adding messages tab to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -51,15 +51,26 @@ export function NavBar() {
               </Link>
             </li>
             {user.isSignedIn && (
-            <li>
-              <Link
-                href="/sell-item"
-                className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Sell Item
-              </Link>
-            </li>
+            <>
+              <li>
+                <Link
+                  href="/messages"
+                  className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Messages
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/sell-item"
+                  className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Sell Item
+                </Link>
+              </li>
+            </>
             )}
             {!user.isSignedIn && (
             <li>
