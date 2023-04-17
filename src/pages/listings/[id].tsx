9c90511af8fdfd3d417bb9d403fdc367d26307e2
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2023-04-17 09:40:26 -0300

    feat: adding form to send messages to seller

--- src/pages/listings/[id].tsx
+++ src/pages/listings/[id].tsx
@@ -1,25 +1,49 @@
+import { useUser } from "@clerk/nextjs";
 import { type NextPage } from "next";
 import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
 import PageHead from "~/components/PageHead";
 import Spinner from "~/components/ui/Spinner";
+import TextAreaInput from "~/components/ui/TextAreaInput";
 import { api } from "~/utils/api";
+
+type SendMessageForm = {
+  message: string
+}
 
 const ListingView: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
+
+  const { register, handleSubmit, reset } = useForm<SendMessageForm>();
+  const sendMessage = api.listings.sendMessage.useMutation()
+  const onSubmit = async (formData: SendMessageForm) => {
+    try {
+      await sendMessage.mutateAsync({
+        listingId: id as string,
+        message: formData.message
+      })
+
+      reset();
+    } catch (error) {
+     console.log(error) 
+    }
+  }
 
   let listing = api.listings.get.useQuery({
     listingId: id as string
   }, {
     enabled: !!id
   });
+  
+  const user = useUser();
 
   if (!listing.data) {
     return (
       <Spinner />
     )
   }
-
+  
   listing = listing.data;
 
   return (
@@ -32,6 +56,24 @@ const ListingView: NextPage = () => {
         <h5 className="mt-8 pl-8 text-2xl bold"> {listing.description} </h5>
         <h5 className="mt-8 pl-8 text-2xl bold"> $ {listing.price} </h5>
 
+        {user.isSignedIn && (
+          <form
+            className="mt-8 flex flex-col gap-4 "
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <TextAreaInput
+              label="Message"
+              name={"message"}
+              register={register}
+            />
+            <button
+              type="submit"
+              className="mb-2 mr-2 rounded-full border border-gray-200 bg-white px-5 py-2.5 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+            >
+              Send Message
+            </button>
+          </form>
+        )}
         </div>
       </main>
     </>
